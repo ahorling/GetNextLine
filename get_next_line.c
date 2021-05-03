ef5d1db0265c48734eaa5ba807bfd5e5938dbf86
Alexander derck Horlings <ahorling@f0r3s26.codam.nl>
2021-05-03 11:58:11 +0200

    About to start testing

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ahorling <ahorling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/27 13:10:23 by ahorling      #+#    #+#                 */
-/*   Updated: 2021/04/27 16:48:39 by ahorling      ########   odam.nl         */
+/*   Updated: 2021/05/03 11:57:26 by ahorling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,31 @@ int find_newline(*char buffer)		//this function serves to check if the buffer ha
 	size_t i;
 
 	i = 0;
-	while (input[i] != '\0')
-		if (input == '\n')
-			return (1);
-		else i++;
+	if (!buffer)
+		return (-1);
+	while (buffer[i] != '\0')
+		if (buffer == '\n')
+			return (1); 
+		i++;
 	return (0);
 }
 
-int clean_buffer(*buffer)
+char	*clean_buffer(*buffer)		//This function serves to remove characters from the buffer that have already been read
 {
+	size_t i;
 
+	i = 0;
+	if (!buffer)
+		return (NULL);
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	if (buffer[i] == '\0')
+		i++;
+	ft_strlcpy(buffer, buffer[i], i + 1);
+	return(buffer);
 }
 
-char	*pull_line(*buffer)
+char	*pull_line(*buffer)		//this function takes the line from the buffer and puts it in a malloc'ed string
 {
 	size_t i;
 	size_t j;
@@ -35,14 +47,14 @@ char	*pull_line(*buffer)
 
 	i = 0;
 	if (!*buffer)
-		return (-1);
+		return (NULL);
 	while (buffer[i] != '\n' && buffer[i])
 			i++
 	str = (char *)malloc((i) * sizeof(char));
 	if (!str)
 	{
 		free(buffer);
-		return (-1);
+		return (NULL);
 	}
 	j = i;
 	i = 0;
@@ -54,22 +66,22 @@ char	*pull_line(*buffer)
 	return (str);
 }
 
-int fill_buffer(char **buffer, int fd)
+int fill_buffer(char **buffer, int fd)		//this function fills the buffer using BUFFER_SIZE, to then be used by pull line
 {
 	int output;
-	char filler[BUFFER_SIZE + 1];
+	char filling[BUFFER_SIZE + 1];
 
 	output = 1;
-	while(find_newline(*buffer) = 0 && output > 0)
+	while(!find_newline(*buffer))
 	{
-		output = read(fd, filler, BUFFER_SIZE);
+		output = read(fd, filling, BUFFER_SIZE);
 		if (output < 0)
 		{
 			if (*buffer)
 				free(buffer);
 			return (-1);
 		}
-		*buffer = ft_buffer_add(*buffer, filler);
+		*buffer = ft_buffer_add(*buffer, filling);
 		if (!*buffer)
 			return (-1);
 	}
@@ -80,8 +92,21 @@ int fill_buffer(char **buffer, int fd)
 
 int	get_next_line(int fd, char **line)
 {
-	static *char = buffer;
+	static char *buffer;
+	int	output;
 
+	output = 1;
 	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
-
+	if (fd < 0 || BUFFER_SIZE <= 0 || !line)
+		return (-1)
+	output = fill_buffer(&buffer, fd);
+	if (output == - 1)
+		return (-1);
+	*line = pull_line(buffer);
+	if (*line == NULL)
+		return (-1);
+	buffer = clean_buffer(buffer);
+	if (output == 0 && buffer != '\0')
+		free (buffer);
+	return (output);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,11 @@
 /*   By: ahorling <ahorling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/27 15:21:01 by ahorling      #+#    #+#                 */
-/*   Updated: 2021/04/27 15:46:47 by ahorling      ########   odam.nl         */
+/*   Updated: 2021/05/03 11:35:40 by ahorling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
 size_t	ft_strlen(const char *str)
 {
@@ -25,6 +27,54 @@ size_t	ft_strlen(const char *str)
 	return (length);
 }
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	srclen;
+
+	if (dest == NULL || src == NULL)
+		return (0);
+	srclen = ft_strlen(src);
+	if (srclen < size)
+		ft_memcpy(dest, src, (srclen + 1));
+	else if (size != 0)
+	{
+		ft_memcpy(dest, src, size - 1);
+		dest[size - 1] = '\0';
+	}
+	return (srclen);
+}
+
+char	*ft_fill_empty(char *src, size_t length)
+{
+	char	*str;
+
+	length++;
+	str = (char *)malloc(length)(sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, src, length);
+	return(str);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t size)
+{
+	size_t	i;
+	char	*output;
+	char	*input;
+
+	i = 0;
+	output = (char *)dest;
+	input = (char *)src;
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		output[i] = input[i];
+		i++;
+	}
+	return (dest);
+}
+
 char	*ft_buffer_add(char const *s1, char const *s2)
 {
 	char	*fullstr;
@@ -33,8 +83,10 @@ char	*ft_buffer_add(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		return (ft_fill_empty(s2, ft_strlen(s2)))
 	fullstr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2));
 	if (fullstr == NULL)
 		return (NULL);
